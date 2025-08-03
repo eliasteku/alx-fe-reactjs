7ed14c5377dbdc684caf17e32243e9bae9e4b5b3
Elias Teku <eliasteku@gmail.com>
2025-08-03 16:51:14 +0300

    Fix basic structure: add components and services folders

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,7 +1,10 @@
+import Header from './components/Header'   // <--- import statement
+
 function App() {
   return (
     <div>
       <h1>GitHub User Search App</h1>
+      <Header />  {/* use the imported component */}
     </div>
   )
 }
---
+++ github-user-search/src/components/Header.jsx
@@ -0,0 +1,5 @@
+function Header() {
+  return <h2>Welcome to GitHub User Search App</h2>
+}
+
+export default Header
