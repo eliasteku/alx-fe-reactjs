c3212b5094d36397797f50144eb3e6503ee37441
Elias Teku <eliasteku@gmail.com>
2025-08-11 20:13:27 +0300

    feat: Task 1

---
+++ tailwind-react-integration/src/App.jsx
@@ -0,0 +1,14 @@
+// src/App.jsx
+
+import UserProfile from './components/UserProfile';
+import './index.css';
+
+function App() {
+  return (
+    <div className="App">
+      <UserProfile />
+    </div>
+  );
+}
+
+export default App;
