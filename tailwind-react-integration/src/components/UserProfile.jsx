798ebaf1e8cf36f90d65e8d2d0c02aa77a464545
Elias Teku <eliasteku@gmail.com>
2025-08-11 20:23:24 +0300

    feat: Task 1 fix

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -2,8 +2,8 @@
 
 function UserProfile() {
   return (
-    // Corrected container with responsive padding
-    <div className="bg-gray-100 p-8 max-w-xs sm:max-w-sm mx-auto my-20 rounded-lg shadow-lg transition-shadow duration-300 ease-in-out hover:shadow-xl sm:p-4 md:p-8">
+    // Corrected container with responsive padding and max-width
+    <div className="bg-gray-100 p-8 max-w-xs mx-auto my-20 rounded-lg shadow-lg transition-shadow duration-300 ease-in-out hover:shadow-xl sm:p-4 md:p-8 md:max-w-sm">
       
       <img
         src="https://via.placeholder.com/150"
@@ -11,7 +11,8 @@ function UserProfile() {
         className="rounded-full w-24 h-24 sm:w-36 sm:h-36 mx-auto transition-transform duration-300 ease-in-out hover:scale-110"
       />
       
-      <h1 className="text-lg sm:text-xl text-blue-800 my-4 text-center transition-colors duration-300 ease-in-out hover:text-blue-500">
+      {/* Corrected heading with responsive font size */}
+      <h1 className="text-lg text-blue-800 my-4 text-center transition-colors duration-300 ease-in-out hover:text-blue-500 sm:text-xl md:text-xl">
         John Doe
       </h1>
       
