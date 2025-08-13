a35cf58b538e5850715b69cf088bae7fec13bae4
Elias Teku <eliasteku@gmail.com>
2025-08-13 20:33:21 +0300

    feat: Add new recipe-sharing-platform project

---
+++ recipe-sharing-platform/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // This line is from your instructions
+    "./public/index.html"        // This line is also from your instructions
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
